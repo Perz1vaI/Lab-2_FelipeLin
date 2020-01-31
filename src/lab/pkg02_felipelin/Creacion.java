c0ed29a4dfc3596c94dad60eccef05db5e5fa46b
Per1vai <felipelinchiu321@gmail.com>
2020-01-31 14:52:20 -0600

    Finalizacion de la clase Creacion y de la opcion 1 del menu

--- src/lab/pkg02_felipelin/Creacion.java
+++ src/lab/pkg02_felipelin/Creacion.java
@@ -1,8 +1,13 @@
 package lab.pkg02_felipelin;
 
+import java.util.Random;
+
 public class Creacion {
 
+    public Random rand = new Random();
+
     // Atributos Modificables
+    private String clase;
     private String raza;
     private String nombre;
     private double estatura;
@@ -12,9 +17,103 @@ public class Creacion {
     private String Nacionalidad;
 
     // Atributos de aspecto jugable
-    private int hp;
-    private int cs;
-    private int ac;
-    private int dg;
+    // HP
+    private int hpMediano = rand.nextInt(10) + 51;
+    private int hpEnano = rand.nextInt(20) + 81;
+    private int hpElfo = rand.nextInt(20) + 51;
+    private int hpHumano = rand.nextInt(35) + 41;
+
+    // CS
+    private int csClerigo = 97;
+    private int csBarbaro = 93;
+    private int csMago = 101;
+    private int csPicaro = 80;
+
+    // AC
+    private int acClerigo = 40;
+    private int acBarbaro = 65;
+    private int acMago = 20;
+    private int acPicaro = 50;
+
+    // DG
+    private int dgClerigo = rand.nextInt(10) + 6;
+    private int dgBarbaro = rand.nextInt(15) + 16;
+    private int dgMago = rand.nextInt(5) + 6;
+    private int dgPicaro = rand.nextInt(10) + 16;
+
+    public Creacion(String clase, String raza, String nombre, double estatura, double peso, int edad, String descripcion, String Nacionalidad) {
+        this.clase = clase;
+        this.raza = raza;
+        this.nombre = nombre;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.edad = edad;
+        this.descripcion = descripcion;
+        this.Nacionalidad = Nacionalidad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getNacionalidad() {
+        return Nacionalidad;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setNacionalidad(String Nacionalidad) {
+        this.Nacionalidad = Nacionalidad;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
 
 }
--- src/lab/pkg02_felipelin/Lab02_FelipeLin.java
+++ src/lab/pkg02_felipelin/Lab02_FelipeLin.java
@@ -1,26 +1,55 @@
 package lab.pkg02_felipelin;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab02_FelipeLin {
-    
+
     public static void main(String[] args) {
         int menu;
         Scanner leer = new Scanner(System.in);
-        
+        ArrayList<Creacion> lista = new ArrayList();
+
         System.out.println("1. Creacion de personaje \n2. Modificar personaje \n3. Ver atributos de un personaje"
                 + "\n4. Eliminar Personaje \n5. Combate \n6. Salida");
         menu = leer.nextInt();
         while (menu != 6) {
             switch (menu) {
                 case 1: {
-                    
+                    System.out.println("1. Clerigo \n2. Barbaro \n3. Mago \n4. Picaro \nDebera de escribir unos de las clases dependiendo que clase quiere");
+                    leer.nextLine();
+                    String clase = leer.nextLine();
+
+                    System.out.println("Asigne el nombre de su personaje");
+                    String nombre = leer.nextLine();
+
+                    System.out.println("1. Mediano \n2. Enano \n3. Elfo \n4. Humano \nDebera de escribir unos de las razas dependiendo que raza quiere");
+                    String raza = leer.nextLine();
+
+                    System.out.println("Ingrese estatura del personaje: ");
+                    double estatura = leer.nextDouble();
+
+                    System.out.println("Ingrese peso del personaje: ");
+                    double peso = leer.nextDouble();
+
+                    System.out.println("Ingrese edad del personaje: ");
+                    int edad = leer.nextInt();
+
+                    System.out.println("Ingrese una descripcion de su personaje: ");
+                    String descripcion = leer.nextLine();
+
+                    System.out.println("1. Norfair \n2. Brinstar \n3. Maridia \n4. Zebes \n5. Crateria \nDebera de escribir unos de las nacionalidades dependiendo que nacionalidad quiere");
+                    String nacionalidad = leer.nextLine();
+
+                    lista.add(new Creacion(clase, raza, nombre, estatura, peso, edad, descripcion, nacionalidad));
+
                     break;
                 }
                 case 2: {
                     break;
                 }
                 case 3: {
+
                     break;
                 }
                 case 4: {
@@ -33,13 +62,15 @@ public class Lab02_FelipeLin {
                     System.exit(0);
                     break;
                 }
-                
+
                 default:
-                    throw new AssertionError();
+                    System.out.println("Error!");
             }
-            
+            System.out.println("1. Creacion de personaje \n2. Modificar personaje \n3. Ver atributos de un personaje"
+                    + "\n4. Eliminar Personaje \n5. Combate \n6. Salida");
+            menu = leer.nextInt();
         }
-        
+
     }
-    
+
 }
