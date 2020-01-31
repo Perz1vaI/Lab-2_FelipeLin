cf8f9e2ef824ab9db64430bf481ccd75479b19dc
Per1vai <felipelinchiu321@gmail.com>
2020-01-31 14:00:36 -0600

    prueba 2

---
+++ src/lab/pkg02_felipelin/Creacion.java
@@ -0,0 +1,20 @@
+package lab.pkg02_felipelin;
+
+public class Creacion {
+
+    // Atributos Modificables
+    private String raza;
+    private String nombre;
+    private double estatura;
+    private double peso;
+    private int edad;
+    private String descripcion;
+    private String Nacionalidad;
+
+    // Atributos de aspecto jugable
+    private int hp;
+    private int cs;
+    private int ac;
+    private int dg;
+
+}
--- src/lab/pkg02_felipelin/Lab02_FelipeLin.java
+++ src/lab/pkg02_felipelin/Lab02_FelipeLin.java
@@ -1,21 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab.pkg02_felipelin;
 
-/**
- *
- * @author felipelinzhao
- */
-public class Lab02_FelipeLin {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Lab02_FelipeLin {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int menu;
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("1. Creacion de personaje \n2. Modificar personaje \n3. Ver atributos de un personaje"
+                + "\n4. Eliminar Personaje \n5. Combate \n6. Salida");
+        menu = leer.nextInt();
+        while (menu != 6) {
+            switch (menu) {
+                case 1: {
+                    
+                    break;
+                }
+                case 2: {
+                    break;
+                }
+                case 3: {
+                    break;
+                }
+                case 4: {
+                    break;
+                }
+                case 5: {
+                    break;
+                }
+                case 6: {
+                    System.exit(0);
+                    break;
+                }
+                
+                default:
+                    throw new AssertionError();
+            }
+            
+        }
+        
     }
     
 }
